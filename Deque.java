d80df2e8b4fe219f310d9289bf06c60affe60438
Kedar Shinde <kedarshinde8011@gmail.com>
2023-04-17 03:06:51 +0530

    In Caffeine We Trust :)

--- Deque.java
+++ Deque.java
@@ -5,6 +5,7 @@
  **************************************************************************** */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
@@ -12,17 +13,27 @@ public class Deque<Item> implements Iterable<Item> {
     private Node<Item> last;
     private int n;
 
-    private int size;
-
     private class Node<Item> {
         private Item item;
         private Node<Item> next;
+        private Node<Item> prev;
     }
 
     public Deque() {
         first = null;
         last = null;
         n = 0;
+    }
+
+    private void checkItem(Item item) {
+        if (item == null)
+            throw new NullPointerException("NULL Bish");
+    }
+
+    private void checkList() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty list man");
+        }
     }
 
     public boolean isEmpty() {
@@ -34,19 +45,92 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public void addFirst(Item item) {
-        if ;
+        checkItem(item);
+
+        Node<Item> old = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.next = old;
+        first.prev = null;
+
+        if (isEmpty()) {
+            last = first;
+        }
+        else old.prev = first;
+
+        n++;
     }
 
     public void addLast(Item item) {
+        checkItem(item);
+
+        Node<Item> old = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        last.prev = old;
+
+        if (isEmpty()) first = last;
+        else old.next = last;
     }
 
     public Item removeFirst() {
+        checkList();
+        Item item = first.item;
+        n--;
+        if (isEmpty()) {
+            first = null;
+            last = null;
+        }
+        else {
+            first = first.next;
+            first.prev = null;
+        }
+        return item;
     }
 
     public Item removeLast() {
+        checkList();
+        Item item = last.item;
+        n--;
+        if (isEmpty()) {
+            first = null;
+            last = null;
+        }
+        else {
+            last = last.prev;
+            last.next = null;
+        }
+        return item;
     }
 
     public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator() {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        public Item next() {
+            if (hasNext()) {
+                Item item = current.item;
+                current = current.next;
+                return item;
+            }
+            else {
+                throw new NoSuchElementException("No such element");
+            }
+        }
+
+
     }
 
     public static void main(String[] args) {
